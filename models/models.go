7f3cf5ddaa1584cb4ea13b24c4c1bb6cc6277e8d
Adam Carter <adam@contra-design.com>
2023-07-14 17:52:29 +0100

    Update models.go

--- models/models.go
+++ models/models.go
@@ -2,9 +2,9 @@ package models
 
 type User struct {
 	ID         uint   `json:"id" gorm:"primaryKey"`
+	StaffID    uint   `json:"staff_id"`
 	Email      string `json:"email" gorm:"unique_index"`
 	Password   string `json:"password"`
 	AdminLevel uint   `json:"admin_level"`
-	Salon      uint   `json:"salon"`
 	Token      string `json:"token" gorm:"-"`
 }
